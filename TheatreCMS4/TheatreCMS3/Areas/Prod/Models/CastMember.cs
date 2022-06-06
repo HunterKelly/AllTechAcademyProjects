af05310755ab64156919d11d1a58d85b963f0d6c
Hunter Kelly <47648040+HunterKelly@users.noreply.github.com>
2022-06-06 14:19:45 -0500

    remove submodule6
    remove submodule6

---
+++ TheatreCMS4/TheatreCMS3/Areas/Prod/Models/CastMember.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Areas.Prod.Models
+{
+    public class CastMember
+    {
+        [Key]
+        public int CastMemberId { get; set; }
+        public string Name { get; set; }
+        public int? YearJoined { get; set; }
+        public string ProductionTitle { get; set; }
+        public PositioinEnum MainRole { get; set; }
+        public string Bio { get; set; }
+        public Byte[] Photo { get; set; }
+        public bool CurrentMember { get; set; }
+        public string Character { get; set; }
+        public int? CastYearLeft { get; set; }
+        public int? DebutYear { get; set; }
+
+    }
+
+    public enum PositioinEnum
+    {
+        Actor, Director, Technician, StageManager, Other
+    }
+}
---
+++ TheatreCMS4/TheatreCMS3/Helpers/TextHelpers.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Helpers
+{
+    public class TextHelpers
+    {
+        public static string CharacterLimiter(string text, int length)
+        {
+                if (string.IsNullOrEmpty(text)) return text;
+            return text.Length <= length ? text : text.Substring(0, length) + "...";
+        }
+
+        public static string LimitWords(string text, int wordLimit)
+        {
+            if (string.IsNullOrEmpty(text)) return text;//check if string is null or empty and return if it is
+            if (wordLimit <= 0) return text;//check if the sentence is even being limited
+            int counter = 0;//used to count the number of words that have passed
+            int place = 0;//used to keep track of what place in the string array the loop is at 
+            bool alreadySpaced = false;//used to check for double spaces
+
+            foreach (char letter in text)
+            {
+                if (letter == ' ')//check for spaces
+                {
+                    if(alreadySpaced == true)//check for double space
+                    {
+                        place++;
+                        continue;
+                    }
+                    alreadySpaced = true;//mark that a space was just found
+                    if (place == 0) //skip this space if the space was at the beginning of the string
+                    { 
+                        place++; 
+                        continue; 
+                    }
+                    counter++;//count the number of words when a space is found
+                    if (counter == wordLimit)//complete the loop if we've reached the wordLimit requested
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    alreadySpaced = false;//mark that the last letter checked was not just a space
+                }
+                place++;
+            }
+            return text.Substring(0, place) + "...";
+        }
+    }
+}
